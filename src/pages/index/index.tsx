48dd697967bf3ffd5d89c245379ee45d4be2bc3e
mudssky <mudssky@gmail.com>
2023-09-16 23:36:08 +0800

    🐳 chore: prettier 导入排序插件，消除ts umd引入报错

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,18 +1,17 @@
-import { AtButton } from "taro-ui";
-import { View, Text } from "@tarojs/components";
-import { useLoad } from "@tarojs/taro";
-import "./index.scss";
-import React from "react";
+import { View } from '@tarojs/components'
+import { useLoad } from '@tarojs/taro'
+import { AtButton } from 'taro-ui'
+import './index.scss'
 
 export default function Index() {
   useLoad(() => {
-    console.log("Page loaded.");
-  });
+    console.log('Page loaded.')
+  })
 
   return (
     <View className="index">
       <AtButton type="primary">按钮文案</AtButton>
       <View>测试首页</View>
     </View>
-  );
+  )
 }
