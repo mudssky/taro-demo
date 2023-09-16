2cb1636cb44f58665b8d8905f9feb2a110d21744
mudssky <mudssky@gmail.com>
2023-09-16 23:18:59 +0800

    ✨ feat:封装请求模块

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -2,6 +2,7 @@ import { AtButton } from "taro-ui";
 import { View, Text } from "@tarojs/components";
 import { useLoad } from "@tarojs/taro";
 import "./index.scss";
+import React from "react";
 
 export default function Index() {
   useLoad(() => {
@@ -11,29 +12,7 @@ export default function Index() {
   return (
     <View className="index">
       <AtButton type="primary">按钮文案</AtButton>
-      <View className="text-[#acc855] text-[100px]">Hello world!</View>
-      <View className="space-y-[1.6rem]">
-        <View className="w-[300rpx] text-black text-opacity-[0.19]">
-          w-[300rpx] text-black text-opacity-[0.19]
-        </View>
-        <View className="min-w-[300rpx] max-h-[100px] text-[20px] leading-[0.9]">
-          min-w-[300rpx] max-h-[100px] text-[20px] leading-[0.9]
-        </View>
-        <View className="max-w-[300rpx] min-h-[100px] text-[#dddddd]">
-          max-w-[300rpx] min-h-[100px] text-[#dddddd]
-        </View>
-        <View className="flex items-center justify-center h-[100px] w-[100px] rounded-[40px] bg-[#123456] bg-opacity-[0.54] text-[#ffffff]">
-          Hello
-        </View>
-        <View className="border-[50px] border-[#098765] border-solid border-opacity-[0.44]">
-          border-[10px] border-[#098765] border-solid border-opacity-[0.44]
-        </View>
-        <View className="grid grid-cols-3 divide-x-[10px] divide-[#010101] divide-solid">
-          <View>1</View>
-          <View>2</View>
-          <View>3</View>
-        </View>
-      </View>
+      <View>测试首页</View>
     </View>
   );
 }
---
+++ src/request/request.ts
@@ -0,0 +1,145 @@
+import Taro from "@tarojs/taro";
+
+// 封装后端返回值类型
+export type ResponseData<T = any> = {
+  code: string | number;
+  msg: string;
+  data: T;
+};
+
+export interface CustomRequestOptions extends Taro.request.Option {
+  baseURL?: string;
+  requestInterceptors?: (config: CustomRequestOptions) => boolean;
+  responseInterceptors?: (res: Taro.request.SuccessCallbackResult<any>) => any;
+  showLoading?: boolean;
+}
+
+export class Request {
+  private baseURL = "/";
+  // 默认配置
+  private baseConfig = {
+    // timeout: 5000,
+    // withCredentials: true,
+  };
+  private baseHeader = {
+    // Authorization: uni.getStorageSync('NEWTOKEN'),
+  };
+  private _config: Partial<CustomRequestOptions>;
+
+  // 覆盖默认配置
+  constructor(config: Partial<CustomRequestOptions>) {
+    this._config = Object.assign(this.baseConfig, config);
+    // console.log('baseUrl', this.baseURL)
+    // console.log('wenrul', webServiceURL);
+
+    if (config.baseURL) {
+      this.baseURL = config.baseURL;
+    }
+    if (config.responseInterceptors) {
+      this.responseInterceptors = config.responseInterceptors;
+    }
+  }
+
+  // 返回false表示不进行请求
+  private requestInterceptors(config: CustomRequestOptions): boolean {
+    return true;
+  }
+
+  private responseInterceptors(
+    res: Taro.request.SuccessCallbackResult<any>
+  ): unknown {
+    const data = res.data as ResponseData;
+    if (data?.code !== 0) {
+      return null;
+    }
+    return data;
+  }
+  // 定义核心请求
+  public async request(config: CustomRequestOptions): Promise<any> {
+    //   拦截器拦截时，不发生请求
+    if (!this.requestInterceptors(config)) {
+      return;
+    }
+
+    return new Promise((resolve, reject) => {
+      Taro.request({
+        ...config,
+        url: encodeURI(config.url),
+        header: {
+          ...this.baseHeader,
+          // Authorization: getAuthToken(),
+          ...config.header,
+        },
+        success: (res) => {
+          const data = this.responseInterceptors(res);
+          if (data) {
+            console.log(
+              `%c${config.url}`,
+              "background:#2d8cf0; padding: 2px; border-radius: 4px;color: #fff;",
+              res.data,
+              {
+                res,
+              }
+            );
+            return resolve(data);
+          }
+        },
+        fail: (res) => {
+          return reject(res);
+        },
+        complete: () => {},
+      });
+    });
+  }
+
+  public async get<T = any>(
+    url: string,
+    config?: Omit<CustomRequestOptions, "url">
+  ): Promise<ResponseData<T>> {
+    return this.request({
+      url: this.baseURL + url,
+      method: "GET",
+      ...config,
+    });
+  }
+
+  public async post<T = any>(
+    url: string,
+    data?: any,
+    config?: CustomRequestOptions
+  ): Promise<ResponseData<T>> {
+    return this.request({
+      url: this.baseURL + url,
+      method: "POST",
+      data: data,
+      ...config,
+    });
+  }
+  public async delete<T = any>(
+    url: string,
+    data?: any,
+    config?: CustomRequestOptions
+  ): Promise<ResponseData<T>> {
+    return this.request({
+      url: this.baseURL + url,
+      method: "DELETE",
+      data: data,
+      ...config,
+    });
+  }
+
+  public async put<T = any>(
+    url: string,
+    data?: any,
+    config?: CustomRequestOptions
+  ): Promise<ResponseData<T>> {
+    return this.request({
+      url: this.baseURL + url,
+      method: "PUT",
+      data: data,
+      ...config,
+    });
+  }
+}
+
+export default new Request({});
